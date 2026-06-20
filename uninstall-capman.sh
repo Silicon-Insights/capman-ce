97be66da31e992cfbf18a804146d20313b2be3d2
bryandickman <45517297+bryandickman@users.noreply.github.com>
2026-06-20 17:31:58 +0100

    Sync capman-ce bundle (v1.0-2923)

--- uninstall-capman.sh
+++ uninstall-capman.sh
@@ -15,7 +15,7 @@ compose() {
   if [ -f .env ]; then
     if command -v docker-compose >/dev/null 2>&1; then
       docker-compose --env-file .env "$@"
-    elif docker --help 2>/dev/null | grep -q '[[:space:]]compose[[:space:]]'; then
+    elif docker compose version >/dev/null 2>&1; then
       docker compose --env-file .env "$@"
     else
       echo "Neither docker compose nor docker-compose is available."
@@ -24,7 +24,7 @@ compose() {
   else
     if command -v docker-compose >/dev/null 2>&1; then
       docker-compose "$@"
-    elif docker --help 2>/dev/null | grep -q '[[:space:]]compose[[:space:]]'; then
+    elif docker compose version >/dev/null 2>&1; then
       docker compose "$@"
     else
       echo "Neither docker compose nor docker-compose is available."
